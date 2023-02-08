b0b5328c30451fa64df34753744cb0a8fed68a39
Matheus <matheus12bh@gmail.com>
2023-02-08 15:58:16 -0800

    add-comments-to-ia-2

--- src/edu/unlv/mis768/ia2/Course.java
+++ src/edu/unlv/mis768/ia2/Course.java
@@ -1,39 +1,54 @@
+/*
+ * Author: Matheus Candido Carvalho
+ * Purpose: The purpose of this program is to create a class named Course that will be used in the Main class. This class
+ * will have private fields for the course number, credits, grade, and points. It will also have a constructor, get and set
+ * methods for the course number, credits, and grade. It will also have a method to convert the grade to points.
+ * Date: 02/08/2023
+ */
+
 package edu.unlv.mis768.ia2;
 
+// Create a class named Course
 public class Course {
+	// Create private fields for the course number, credits, grade, and points
 	private String number;
 	private int credits;
 	private String grade;
 	private double points;
 	
+	// Create a constructor
 	public Course() {}
 
+	// Create get methods for the course number
 	public String getNumber() {
 		return number;
 	}
-
+	// Create set methods for the course number
 	public void setNumber(String number) {
 		this.number = number;
 	}
-
+	// Create get methods for the credits
 	public int getCredits() {
 		return credits;
 	}
-
+	// Create set methods for the credits
 	public void setCredits(int credits) {
 		this.credits = credits;
 	}
-
+	// Create get methods for the grade
 	public String getGrade() {
 		return grade;
 	}
-
+	// Create set methods for the grade
 	public void setGrade(String grade) {
 		this.grade = grade;
+		// Call the convertGradeToPoints method when the grade is set
 		convertGradeToPoints(grade);
 	}
 	
+	// Create a method to convert the grade to points
 	private void convertGradeToPoints(String grade) {
+		// Use if statements to convert the grade to points
 		if(grade.equals("A") || grade.equals("A+")) {
 			points = 4.0;
 		} else if (grade.equals("A-")) {
@@ -58,7 +73,7 @@ public class Course {
 			points = 0.0;
 		}
 	}
-	
+	// Create a get method for the points
 	public double getPoints() {
 		return points;
 	}
--- src/edu/unlv/mis768/ia2/individual_assignment_2.java
+++ src/edu/unlv/mis768/ia2/individual_assignment_2.java
@@ -8,109 +8,134 @@
 
 package edu.unlv.mis768.ia2;
 
+// Importing the necessary libraries
 import java.util.ArrayList;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
-
+// Create main class
 public class individual_assignment_2 {
 
+	// Instantiating the decimal format object globally to be used in the whole program
     static DecimalFormat df = new DecimalFormat("#.##");
 
+	// Create main method
 	public static void main(String[] args) throws IOException {
-
+		// Instantiating the Scanner object to read user input
 		Scanner kb = new Scanner(System.in);
 		
+		// Create a variable to store the user input and a counter to keep track of the number of courses
 		char insertCourse;
 		int counter = 1;
-
+		// Print the welcome message
 		System.out.println("This program prints the transcript and calculates the GPA, based on the courses entered");
 		System.out.println("Please enter the courses below.");
 		
+		// Create an ArrayList of the Class Course to store the courses 
 		ArrayList<Course> courses = new ArrayList<Course>();
-		
+		// Create a do-while loop to ask the user if he wants to insert more courses
 		do {
+			// Create a new object of the Class Course
 			Course course = new Course();
 			
+			// Ask the user to enter the course number
 			System.out.print("Number of course " + counter +  ":");
 			course.setNumber(kb.next().toUpperCase());
 			
-
+			// Ask the user to enter the credit hours of the course
 			System.out.print("Credit Hours of course " + counter +  ":");
 			course.setCredits(kb.nextInt());
 			
-
+			// Ask the user to enter the letter grade of the course
 			System.out.print("Letter grade of course " + counter +  ":");
 			course.setGrade(kb.next().toUpperCase());
 			
+			// Add the course to the ArrayList
 			courses.add(course);
 			
-			
+			// Ask the user if he wants to insert more courses
 			System.out.println("More courses? (Y/N)");
+			// Store the user input in the variable insertCourse
 			insertCourse = kb.next().toUpperCase().charAt(0);
-			
+			// Increment the counter
 			counter++;
-			
+		// Repeat the loop while the user wants to insert more courses
 		} while(insertCourse == 'Y');
 		
+		// Createq a variable to store the GPA and call the method to calculate the GPA
 		double gpa = calculateGPA(courses);
 		
-		System.out.print("The GPA is " + df.format(gpa));
+		// Print the GPA
+		System.out.println("The GPA is " + df.format(gpa));
 		
+		// Create a variable to store the user input and ask the user if he wants to save the results to a file
 		char saveInFile;
 		
+		// Ask the user if he wants to save the results to a file
 		System.out.print("Would you like to save the result to a file (Y/N)");
 		
+		// Store the user input in the variable saveInFile
 		saveInFile = kb.next().toUpperCase().charAt(0);
 		
+		// If the user wants to save the results to a file, call the method to generate the file
 		if(saveInFile == 'Y') {
+			// Create a variable to store the file name
 			String fileName;
+			// Ask the user to enter the file name
 			System.out.print("Please enter the file name for the transcript:");
+			// Store the user input in the variable fileName
 			fileName = kb.next();
-			
+			// Call the method to generate the file
 			generateFileWithRecords(courses, fileName, gpa);
-			
 		}
-		
-		
-		
 	}
-	
+	// Create a method to generate the file with the results
 	private static void generateFileWithRecords(ArrayList<Course> courses, String fileName, double gpa) throws IOException  {
+		// Create a PrintWriter object to write to the file
 		PrintWriter outputFile = new PrintWriter(fileName);
-
+		
+		// Print the header of the file
 		outputFile.println("COURSE\t\tCREDIT\t\tGRADE\t\tGRADE POINT");
 		
+		// Loop through the ArrayList and print the information of each course
 		courses.forEach((course) -> outputFile.println(course.getNumber()+"\t\t\t"+course.getCredits()+"\t\t\t"+course.getGrade()+"\t\t\t"+course.getPoints()));
-		
+		// Print the footer of the file
 		outputFile.println("=========================");
+		// Print the GPA
 		outputFile.println("GPA: " + df.format(gpa));
+		// Close the file
 		outputFile.close();
 		
 	}
 	
+	// Create a method to calculate the GPA
 	private static double calculateGPA(ArrayList<Course> courses) {
+		// Create a variable to store the sum of the points times the credits
 		double sumOfPointsTimesCredits = 0.00;
 		
+		// Create a variable to store the total credit hours
 		int totalCreditHours = 0;
 		
-		
-		
+		// Loop through the ArrayList and calculate the sum of the points times the credits and the total credit hours
 		for(int i = 0; i < courses.size(); i++) {
+			// Check if the course has credit hours
 			if(courses.get(i).getCredits() > 0) {
+				// Add the credit hours to the total credit hours
 				totalCreditHours += courses.get(i).getCredits();
+				// Add the points times the credits to the sum of the points times the credits
 				sumOfPointsTimesCredits += courses.get(i).getPoints() * courses.get(i).getCredits();
 			} 
 		}
-		
+		// Check if the total credit hours is 0, if it is, return 0.00
 		if(totalCreditHours == 0) return 0.00;
 		
+		// Calculate the GPA
 		double gpa = sumOfPointsTimesCredits / totalCreditHours;
-		
+
+		// Return the GPA
 		return gpa;
-		
 	}
 
 }
