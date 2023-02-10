b827f1207f3f02aea57ae117329001111d79cce2
Matheus <matheus12bh@gmail.com>
2023-02-09 17:02:16 -0800

    start-exercise-3

---
+++ src/edu/unlv/mis768/ce3/Test.java
@@ -0,0 +1,38 @@
+package edu.unlv.mis768.ce3;
+
+public class Test {
+	private int numQuestion;
+	private int numMissed;
+
+	public Test(int numQ) {
+		this.numQuestion = numQ;
+	}
+	
+	public int getNumQuestion() {
+		return numQuestion;
+	}
+
+	public void setNumMissed(int numMissed) {
+		this.numMissed = numMissed;
+	}
+
+	public int getNumMissed() {
+		return numMissed;
+	}
+	
+	public double getPointsEach() {
+		return 0.00;
+	}
+	
+	public double getScore() {
+		return 0.00;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/edu/unlv/mis768/ce3/class_exercise_3.java
@@ -0,0 +1,22 @@
+package edu.unlv.mis768.ce3;
+
+import java.util.Scanner;
+
+public class class_exercise_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner kb = new Scanner(System.in);
+		
+		System.out.print("Please enter the nunmber of quesitons for the first test: ");
+		System.out.print("Please enter the nunmber of quesitons missed for the first test: ");
+		
+		System.out.println("The test-maker missed 5 question(s).");
+		System.out.println("The score is 72.22");
+		
+
+		System.out.println("The score of the test tests are not the same");
+	}
+
+}
